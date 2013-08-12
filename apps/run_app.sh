07b211d9d81a32e46248a2be60e34c91686d3cbd
Johannes Schmitz <schmitz@ti.rwth-aachen.de>
2013-08-12 14:30:02 +0200

    Fix relative paths in run_app.

--- apps/run_app.sh
+++ apps/run_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export LD_LIBRARY_PATH="$PWD/../build/lib:/home/zivkovic/git/gr-zmqblocks/build/lib"
-export PYTHONPATH="$PWD/../build/swig:$PWD/../python:/home/zivkovic/git/gr-zmqblocks/build/swig:/home/zivkovic/git/gr-zmqblocks/python"
+export LD_LIBRARY_PATH="$PWD/../build/lib:$PWD/../../gr-zmqblocks/build/lib"
+export PYTHONPATH="$PWD/../build/swig:$PWD/../python:$PWD/../../gr-zmqblocks/build/swig:$PWD/../../gr-zmqblocks/python"
 #/usr/bin/python $1
 
 ok=yes

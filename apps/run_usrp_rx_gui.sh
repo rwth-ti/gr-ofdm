4f4ad583127c07ea4f5c3a45e536f22c2560e979
Johannes Schmitz <schmitz@ti.rwth-aachen.de>
2017-08-21 14:38:04 +0200

    Remove lo-offset argument in run scripts to circumvent issue with USRP B2x0

--- apps/run_usrp_rx_gui.sh
+++ apps/run_usrp_rx_gui.sh
@@ -18,6 +18,6 @@ do
 done
 echo $TXHOSTNAME
 
-rx.py --disable-ctf-enhancer --bandwidth=1M --subcarriers=200 --fft-length=256 --scatterplot --ber-window=50000 --freq=2.45G --lo-offset=4M $*
 ofdm_rx_gui.py $TXHOSTNAME &
 
+rx.py --disable-ctf-enhancer --bandwidth=1M --subcarriers=200 --fft-length=256 --scatterplot --ber-window=50000 --freq=2.45G $*
